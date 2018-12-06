244c048739071dc87eddccec4dd9574944752b97
adjadtea <adjadtea@gmail.com>
2018-12-06 18:07:07 +0700

    remove library tcpdf

--- application/libraries/Mypdf.php
+++ application/libraries/Mypdf.php
@@ -1,5 +1,4 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-include_once(dirname(__FILE__).'/tcpdf/tcpdf.php');
 class Mypdf extends TCPDF{
 	private $headerText;
 	public function __construct($param){
